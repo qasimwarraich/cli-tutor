58b329899fd3d4d811ee3f0a699798156ed5a2f5
qasimwarraich <qasim.warraich@gmail.com>
2022-04-20 18:51:03 +0200

    feat(lessons): First implementation of generalising lessons

--- src/main/main.go
+++ src/main/main.go
@@ -5,45 +5,35 @@ import (
 	"os/exec"
 	"time"
 
+	"cli-tutor/src/input"
+	"cli-tutor/src/lesson"
 	"cli-tutor/src/printer"
 	"cli-tutor/src/prompt"
-	"cli-tutor/src/input"
+	"cli-tutor/src/task"
 
 	"github.com/chzyer/readline"
 	"github.com/muesli/termenv"
 )
 
-
-
-
 func main() {
-	rl, err := readline.New(prompt.BuildPrompt() + " > ")
+	lesson1 := new(lesson.Lesson)
 
-	if err != nil {
-		panic(err)
+	lesson1.Name = "What is textual interaction"
+	lesson1.Vocabulary = []string{"pwd", "ls", "cd", "whoami", "uname", "echo", "curl"}
+	lesson1.Description = "This lesson will teach you about the basics of command line interaction"
+	lesson1.Tasks = []task.Task{
+		{
+			Description: "Some spam that will eventually be a task description.",
+		},
 	}
-	defer rl.Close()
 
 	termenv.ClearScreen()
-
 	printer.Print("Welcome to Chistole", "welcome")
-
 	time.Sleep(1 * time.Second)
 
-	printer.Print("This lesson will be about", "")
-	printer.Print(`Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-    reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-    exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
-    nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-    minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
-    elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
-    Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing
-    id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
-    reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate
-    laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa
-    duis.`, "")
-
-	// time.Sleep(4 * time.Second)
+	printer.Print("This lesson is titled:", "")
+	printer.Print(lesson1.Name, "")
+	printer.Print("\n\n"+lesson1.Description, "")
 
 	printer.Print("\n\nWhen you are ready press any key to begin", "note")
 	fmt.Scanln() // Any key
@@ -51,8 +41,13 @@ func main() {
 	termenv.ClearScreen()
 
 	printer.Print("Welcome to the shell", "")
-	printer.Print(`Try out some commands or type "exit"/"quit" to quit the shell`, "note")
+	printer.Print("Try out some commands or type 'exit'/'quit' to quit the shell", "note")
 
+	rl, err := readline.New(prompt.BuildPrompt() + " > ")
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
 	// Readline loop
 	for {
 		line, err := rl.Readline()
@@ -63,12 +58,11 @@ func main() {
 			continue
 		}
 
-        if line == "quit" || line == "exit" {
-            break
-        }
+		if line == "quit" || line == "exit" {
+			break
+		}
 
-        printer.Print("spam", "")
-		command := input.InputFilter(line)
+		command := input.InputFilter(line, lesson1.Vocabulary)
 
 		var cmd *exec.Cmd
 
