380965a42600df40defd072966153f0d772b6406
qasimwarraich <qasim.warraich@gmail.com>
2022-04-27 12:02:16 +0200

    feat(lesson1): Adds more content and tips to lesson 1

--- src/lessons/lesson1.go
+++ src/lessons/lesson1.go
@@ -19,19 +19,36 @@ func GetLesson() lesson.Lesson {
     cwd_styled := styler.Style(cwd, "tip")
 
 
-    step1 := fmt.Sprintf(`
-    Welcome to the command line, this may look intimidating but this tutorial
+    helpStep := fmt.Sprintf(
+`    Welcome to the command line, this may look intimidating but this
+    tutorial will introduce you to the concepts you need to be familiar
+    with to thrive at the command line.
+
+    When you are ready enter "%s" or "%s" to procede to the next task.`,
+    styler.Style("next",  "tip"), styler.Style("n",  "tip"))
+
+
+
+    promptStep := fmt.Sprintf(
+`    Welcome to the command line, this may look intimidating but this tutorial
     will introduce you to the conceptd you need to be familiar with to thrive
     at the command line. 
-
+    
     This funny looking line below you is called the "prompt".
-
+    
     It displays information about the: 
-
+    
     current user = %s <-- You!
     host machine = %s <-- Your machine
     the current directory you are in = %s <-- Where on you are on your machine.
-    `, user_styled, host_styled, cwd_styled)
+`, user_styled, host_styled, cwd_styled)
+    
+    cmdStep := fmt.Sprintf(
+`    Try it out for yourself!
+    type %s and look at the result
+`, styler.Style("whoami", "tip"))
+
+    
 
     lesson1 := new(lesson.Lesson)
     lesson1.Name = "What is textual interaction?"
@@ -39,13 +56,13 @@ func GetLesson() lesson.Lesson {
     lesson1.Description = "This lesson will teach you about the basics of command line interaction"
     lesson1.Tasks = []task.Task{
         {
-            Description: step1,
+            Description: helpStep,
         },
         {
-            Description:  "Some spam that will eventually be a task description.",
+            Description: promptStep,
         },
         {
-            Description: "Some spam that will eventually be a task description.",
+            Description: cmdStep,
         },
         {
             Description: "Last spam",
