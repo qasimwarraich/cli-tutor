8958926fb5652f417432a3e563a0c42622409ff2
qasimwarraich <qasim.warraich@gmail.com>
2022-05-11 14:26:19 +0200

    feat: Make task a part of the lesson package

--- src/lesson/lesson.go
+++ src/lesson/lesson.go
@@ -1,11 +1,9 @@
 package lesson
 
-import "cli-tutor/src/task"
-
 
 type Lesson struct {
     Name string
     Vocabulary []string
     Description string
-    Tasks []task.Task
+    Tasks []Task
 }
