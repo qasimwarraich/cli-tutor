75cfd8607117d23bbf61b804113c8f505988c1b3
qasimwarraich <qasim.warraich@gmail.com>
2022-05-17 17:28:49 +0200

    refactor: Abstracts filtering and command running to input package

--- src/input/input.go
+++ src/input/input.go
@@ -1,13 +1,29 @@
 package input
 
-import "strings"
+import (
+	"os/exec"
+	"strings"
+)
 
 func InputFilter(s string, vocabulary []string) []string {
 	split := strings.Fields(s)
-    if len(split) > 0 && contains(split[0], vocabulary) {
+	if len(split) > 0 && contains(split[0], vocabulary) {
 		return split
 	}
 	return []string{}
+}
+
+func RunCommand(filtered_input []string) string {
+	var cmd *exec.Cmd
+
+	if len(filtered_input) > 1 {
+		args := filtered_input[1:]
+		cmd = exec.Command(filtered_input[0], args...)
+	} else {
+		cmd = exec.Command(filtered_input[0])
+	}
+	output, _ := cmd.CombinedOutput()
+	return string(output)
 }
 
 func contains(s string, arr []string) bool {
