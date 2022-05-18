01c58560dab9539095908e7ae59a700b52f79064
qasimwarraich <qasim.warraich@gmail.com>
2022-05-18 14:01:19 +0200

    test: Updates parser_test to be more generic

--- src/lesson/parser_test.go
+++ src/lesson/parser_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestParseLesson(t *testing.T) {
-	temp := template.Must(template.New("lesson1.md").Funcs(FuncMap).ParseFiles("../../lessons/lesson1.md"))
+	temp := template.Must(template.New("test.md").Funcs(FuncMap).ParseFiles("./test/test.md"))
 	f, _ := os.Create("expanded.md")
 	defer os.Remove(f.Name())
 
@@ -18,9 +18,9 @@ func TestParseLesson(t *testing.T) {
 		log.Panic(err)
 	}
 
-	spam, _ := os.ReadFile("expanded.md")
+	expandedTestFile, _ := os.ReadFile("expanded.md")
 
-	result := Lesson{Name: "Basics of Textual Interaction", Vocabulary: []string{"pwd", "ls", "cd", "whoami", "uname", "echo", "curl", "man", "clear", "less", "vim"}, Description: "This lesson will introduce you to the very basics of command line interaction.\nWe will introduce you to the tutor program and how to formulate a command.", Tasks: []Task{{Title: "Introduction", Description: "Welcome to the command line, this may look intimidating but, this  \ntutorial will introduce you to the concepts you need to be familiar  \nwith to thrive at the command line.\n\nWhen you are ready type `n` or `next` and hit enter to continue.", Expected: ""}, {Title: "What is the Shell?", Description: "Textual interaction is a form of interacting with your computer's operating\nsystem using textual input. You, the user, can issue `commands` and the `shell`\nwill interpret them and produce an output.\n\nThe `shell` is a program that wraps your operating system and acts an\nintermediary between the user and the operating system. It manages the user's\ninteraction by accepting input in the form of `commands` and relays output in the\nform of produced output, errors or special shell features like shortcuts.\n```txt\n                       shell───────────────────────┐\n                       │     interpret             │\n┌──────┐               │     ▲     │               │\n│ user ├─command──────►├─────┘     │       ┌────┐  │\n└───▲──┘               │           └──────►│ os │  │\n    │                  │                   └──┬─┘  │\n    └─────output───────┤                      │    │\n                       │◄──output─────────────┘    │\n                       │                           │\n                       └───────────────────────────┘\n\n```\n\nThis graph is an illustration of the interaction cycle between the user and shell.\n\nType `n` or `next` and hit enter to continue.", Expected: ""}, {Title: "Command Formulation", Description: "Commands can generally be supplied to the shell by using the following format:\n\n`command` `--flags` `arguments`\n\ne.g. `wc --lines file.txt `\n\nA `command` is actually a program the shell can run. In the above example the program being called is `wc`, a word counting program.\n\n`--flags` are additional cues that can be provided to the program to alter its behaviour. These can be optional, but almost all command line programs have flags to help produce the output the user wants. In the above example we supply the `--lines` flag to let `wc` know we are only interested in the line count and not the rest of the information the program can produce.\n\nFinally, we have the `argument` which is the input to the program, in the case of our example, a file named `file.txt`.\n\nType `n` or `next` and hit enter to continue.", Expected: ""}, {Title: "Try a Command", Description: "Try it out for yourself!\nType the command `whoami`, hit Enter and look at the result.", Expected: "spam"}, {Title: "Prompt", Description: "This funny looking line below you is called the `prompt`.\n\nIt displays information about the:\n\ncurrent user = `spam`← You!\nhost machine = `spam-linux` ← Your machine\nthe current directory you are in = `/home/spam/Programming/Go/cli-tutor/src/lesson` ← Where on you are on your machine.", Expected: ""}}}
+	result := Lesson{Name: "Lesson Title", Vocabulary: []string{"TEST", "echo"}, Description: "This should get parsed as the lesson description.", Tasks: []Task{{Title: "First Task", Description: "Will this be parsed.\nHopefully!\n\nWill this nested part get parsed?\n\nWill this `code` render.\nand what about this expansion: TEST", Expected: ""}, {Title: "Does the graph work?", Description: "Does it (This title paragraph is necessary)\n```txt\n                       shell───────────────────────┐\n                       │     interpret             │\n┌──────┐               │     ▲     │               │\n│ user ├─command──────►├─────┘     │       ┌────┐  │\n└───▲──┘               │           └──────►│ os │  │\n    │                  │                   └──┬─┘  │\n    └─────output───────┤                      │    │\n                       │◄──output─────────────┘    │\n                       │                           │\n                       └───────────────────────────┘\n\n```\n\nDoes this following paragraph also render?", Expected: ""}, {Title: "Check Expected", Description: "Tests if expected value is parsed correctly. Type `echo TEST`", Expected: "TEST"}}}
 
 	type args struct {
 		content []byte
@@ -31,8 +31,8 @@ func TestParseLesson(t *testing.T) {
 		want Lesson
 	}{
 		{
-			"Lesson 1 Parse",
-			args{spam},
+			"Parser Test: test/test.md",
+			args{expandedTestFile},
 			result,
 		},
 	}
