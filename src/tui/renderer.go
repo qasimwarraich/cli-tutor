ca8f6950b147a1edb7c7ec2d8933f86b72eb9ed3
qasimwarraich <qasim.warraich@gmail.com>
2022-05-23 14:20:37 +0200

    refactor(tui): create tui package
    - Welcome message moved.
    - Markdown renderer definition moved.

---
+++ src/tui/renderer.go
@@ -0,0 +1,12 @@
+package tui
+
+import "github.com/charmbracelet/glamour"
+
+func GetRenderer() *glamour.TermRenderer {
+	r, _ := glamour.NewTermRenderer(
+		glamour.WithPreservedNewLines(),
+		glamour.WithStandardStyle("dark"),
+		glamour.WithWordWrap(130),
+	)
+	return r
+}
