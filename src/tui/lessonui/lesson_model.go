b844c14f8702f3564b91c26f87706350b0238659
qasimwarraich <qasim.warraich@gmail.com>
2022-06-21 13:55:19 +0200

    feat: Big ui update
    This commit creates a hierarchy of models. With the menu being the
    default one. Presently the menu is working, but it needs to be linked to
    the lesson view appropriately.
    
    TODO:
    - Parser for lesson titles and descriptions.
    - Link between lesson and menu views.

---
+++ src/tui/lessonui/lesson_model.go
@@ -0,0 +1,133 @@
+package lessonui
+
+import (
+	"fmt"
+	"log"
+
+	"cli-tutor/src/input"
+	"cli-tutor/src/lesson"
+	"cli-tutor/src/printer"
+	"cli-tutor/src/prompt"
+	"cli-tutor/src/tui/tuihelpers"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/chzyer/readline"
+)
+
+type BackMsg bool
+
+type LessonModel struct {
+	currentLesson lesson.Lesson
+	rl            *readline.Instance
+	r             *glamour.TermRenderer
+	quitting      bool
+}
+
+func New(choice string) LessonModel {
+	renderer := tuihelpers.GetRenderer()
+
+	// Init Readline
+	rl, err := readline.New(prompt.BuildPrompt() + " > ")
+	if err != nil {
+		panic(err)
+	}
+	// defer rl.Close() // Need to look into this, when it is set it loops indefinitely.
+	return LessonModel{
+		currentLesson: lesson.LoadLesson(choice),
+		rl:            rl,
+		r:             renderer,
+		quitting:      false,
+	}
+}
+
+func (m LessonModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m LessonModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if !m.quitting {
+		m.rline()
+	} else {
+		return m, func() tea.Msg {
+			return BackMsg(true)
+		}
+	}
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m LessonModel) View() string {
+	s := ""
+
+	return s
+}
+
+func (m *LessonModel) rline() {
+	// Readline loop
+	currentTask := 0
+	for {
+		log.Print(m.rl.Config.Prompt)
+		if currentTask < 0 {
+			currentTask = 0
+		}
+
+		if currentTask >= len(m.currentLesson.Tasks) {
+			printer.Print("All Task Completed!, Exiting tutor!", "note")
+			break
+		}
+
+		tracker := fmt.Sprintf("\n\n%s : %s [%d/%d]:", m.currentLesson.Name, m.currentLesson.Tasks[currentTask].Title, currentTask, len(m.currentLesson.Tasks)-1)
+		printer.Print(tracker, "guide")
+
+		out, _ := m.r.Render(m.currentLesson.Tasks[currentTask].Description)
+		printer.Print(out, "")
+
+		line, err := m.rl.Readline()
+		log.Print(line)
+		if err != nil { // io.EOF
+			break
+		}
+
+		if line == "" {
+			continue
+		}
+
+		if line == "next" || line == "n" {
+			currentTask++
+			continue
+		}
+
+		if line == "prev" || line == "p" {
+			currentTask--
+			continue
+		}
+
+		if line == "quit" || line == "exit" {
+			printer.Print("Exiting tutor, Good bye!", "note")
+			m.quitting = true
+			break
+		}
+
+		filtered_input := input.InputFilter(line, m.currentLesson.Vocabulary)
+		if len(filtered_input) == 0 {
+			printer.Print("Let's stick to the basics", "error")
+			continue
+		}
+
+		/* As a final step if the command is in our allowed vocabulary we run
+		 * it as a system call, display it's output and validate it against the
+		 * expected value on the lesson if it exists. */
+		output := input.RunCommand(filtered_input)
+		printer.Print(string(output), "")
+		input.ValidateCommand(output, m.currentLesson, &currentTask)
+
+	}
+}
--- src/tui/tui.go
+++ src/tui/tui.go
@@ -2,142 +2,92 @@ package tui
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"cli-tutor/src/input"
-	"cli-tutor/src/lesson"
-	"cli-tutor/src/printer"
-	"cli-tutor/src/prompt"
+	"cli-tutor/src/tui/lessonui"
+	"cli-tutor/src/tui/menuui"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/glamour"
-	"github.com/chzyer/readline"
 )
 
-type model struct {
-	currentLesson lesson.Lesson
-	rl            *readline.Instance
-	r             *glamour.TermRenderer
-	quitting      bool
+type sessionState int
+
+const (
+	menuView sessionState = iota
+	lessonView
+)
+
+type MainModel struct {
+	state      sessionState
+	menu       tea.Model
+	lesson     tea.Model
+	quitting   bool
+	windowsize tea.WindowSizeMsg
 }
 
-func initialModel() model {
-	// currentLesson := lesson.LoadLesson()
-
-	// PrintWelcome(currentLesson)
-	renderer := GetRenderer()
-
-	// Init Readline
-	rl, err := readline.New(prompt.BuildPrompt() + " > ")
-	if err != nil {
-		panic(err)
-	}
-	// defer rl.Close()
-	return model{
-		currentLesson: lesson.LoadLesson(),
-		rl:            rl,
-		r:             renderer,
-		// quitting:      false,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	PrintWelcome(m.currentLesson)
+func (m MainModel) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if !m.quitting {
-		m.rline()
-	} else {
-		return m, tea.Quit
-	}
+func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    var cmds []tea.Cmd
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		}
+	case tea.WindowSizeMsg:
+		m.windowsize = msg // pass this along to the entry view so it uses the full window size when it's initialized
+
+	case lessonui.BackMsg:
+		m.state = menuView
 	}
 
-	return m, nil
+	switch m.state {
+	case menuView:
+		menu, cmd := m.menu.Update(msg)
+        cmds = append(cmds, cmd)
+		menuModel, ok := menu.(menuui.MenuModel)
+		if !ok {
+			panic("something went wrong with the menu ui ")
+		}
+
+		m.menu = menuModel
+
+	case lessonView:
+		m.lesson.Update(msg)
+		// lesson, _ := m.lesson.Update(msg)
+		// lessonModel, ok := lesson.(lessonui.LessonModel)
+		// if !ok {
+		// 	panic("something went wrong with the lesson ui ")
+		// }
+		// m.menu = lessonModel
+	}
+	return m, tea.Batch(cmds...)
 }
 
-func (m model) View() string {
-	s := ""
+func (m MainModel) View() string {
+	switch m.state {
 
-	// s += "\nPress q to quit.\n"
+	case lessonView:
+		return m.lesson.View()
 
-	return s
+	default:
+		return m.menu.View()
+
+	}
+}
+
+func New() MainModel {
+	return MainModel{
+		state:      menuView,
+		menu:       menuui.New(),
+		lesson:     nil,
+		quitting:   false,
+		windowsize: tea.WindowSizeMsg{},
+	}
 }
 
 func StartUI() {
-	p := tea.NewProgram(initialModel())
-	if err := p.Start(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+	m := New()
+	if err := tea.NewProgram(m).Start(); err != nil {
+		fmt.Println("Error running program:", err)
 		os.Exit(1)
-	}
-}
-
-func (m *model) rline() {
-	// Readline loop
-	currentTask := 0
-	for {
-		log.Print(m.rl.Config.Prompt)
-		if currentTask < 0 {
-			currentTask = 0
-		}
-
-		if currentTask >= len(m.currentLesson.Tasks) {
-			printer.Print("All Task Completed!, Exiting tutor!", "note")
-			break
-		}
-
-		tracker := fmt.Sprintf("\n\n%s : %s [%d/%d]:", m.currentLesson.Name, m.currentLesson.Tasks[currentTask].Title, currentTask, len(m.currentLesson.Tasks)-1)
-		printer.Print(tracker, "guide")
-
-		out, _ := m.r.Render(m.currentLesson.Tasks[currentTask].Description)
-		printer.Print(out, "")
-
-		line, err := m.rl.Readline()
-		log.Print(line)
-		if err != nil { // io.EOF
-			break
-		}
-
-		if line == "" {
-			continue
-		}
-
-		if line == "next" || line == "n" {
-			currentTask++
-			continue
-		}
-
-		if line == "prev" || line == "p" {
-			currentTask--
-			continue
-		}
-
-		if line == "quit" || line == "exit" {
-			printer.Print("Exiting tutor, Good bye!", "note")
-			m.quitting = true
-			break
-		}
-
-		filtered_input := input.InputFilter(line, m.currentLesson.Vocabulary)
-		if len(filtered_input) == 0 {
-			printer.Print("Let's stick to the basics", "error")
-			continue
-		}
-
-		/* As a final step if the command is in our allowed vocabulary we run
-		 * it as a system call, display it's output and validate it against the
-		 * expected value on the lesson if it exists. */
-		output := input.RunCommand(filtered_input)
-		printer.Print(string(output), "")
-		input.ValidateCommand(output, m.currentLesson, &currentTask)
-
 	}
 }
