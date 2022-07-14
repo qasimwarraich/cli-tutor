a604458db4626d180e69ff743bf712b848d149d4
qasimwarraich <qasim.warraich@gmail.com>
2022-07-14 22:23:14 +0200

    feat: Add custom styling to markdown renderer

--- src/tui/tuihelpers/renderer.go
+++ src/tui/tuihelpers/renderer.go
@@ -5,7 +5,7 @@ import "github.com/charmbracelet/glamour"
 func GetRenderer() *glamour.TermRenderer {
 	r, _ := glamour.NewTermRenderer(
 		glamour.WithPreservedNewLines(),
-		glamour.WithStandardStyle("dark"),
+		glamour.WithStylePath("./custom_style.json"),
 		glamour.WithWordWrap(130),
 	)
 	return r
