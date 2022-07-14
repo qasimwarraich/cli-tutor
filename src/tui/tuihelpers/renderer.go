2ffef11ed9cd00f0ae8b6ebfcc81f8001990fad9
qasimwarraich <qasim.warraich@gmail.com>
2022-07-15 01:04:00 +0200

    fix: Reverts custom styling for renderer.
    Pathing issues need to be addressed.

--- src/tui/tuihelpers/renderer.go
+++ src/tui/tuihelpers/renderer.go
@@ -5,7 +5,8 @@ import "github.com/charmbracelet/glamour"
 func GetRenderer() *glamour.TermRenderer {
 	r, _ := glamour.NewTermRenderer(
 		glamour.WithPreservedNewLines(),
-		glamour.WithStylePath("./custom_style.json"),
+        // glamour.WithStylePath("./custom_style.json") TODO: This needs to be embedded into the binary.
+		glamour.WithStandardStyle("dark"),
 		glamour.WithWordWrap(130),
 	)
 	return r
