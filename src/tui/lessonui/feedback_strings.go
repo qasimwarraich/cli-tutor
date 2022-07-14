71cb0f5d7895c7e457b9f34d725a779ccd1f5fc3
qasimwarraich <qasim.warraich@gmail.com>
2022-07-14 21:44:03 +0200

    refactor: Creates a constants file for feedback messages

---
+++ src/tui/lessonui/feedback_strings.go
@@ -0,0 +1,7 @@
+package lessonui
+
+const (
+	invalid_command string = "Let's stick to the basics"
+	exiting         string = "Exiting tutor, Good bye!"
+	expected        string = "This task is interactive, try to practice the command before proceeding."
+)
