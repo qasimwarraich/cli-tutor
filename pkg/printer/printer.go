6ad8936a2b65a5b6f46aafeb983f05616a08ea86
DMunkei <stareye863@gmail.com>
2024-03-24 01:24:17 +0100

    wip: Add colour palette
    - This introduces a new colours package. It's intended to store all of
      the colour definitions used in the application.
    
    - This is still WIP, the thoughts behind this is to refactor some of the
      hard coded colour codes into named literals. This could lead to users
      defining their own colourschemes/setting which colours should be used
      for which sort of text. Another possible addition are themes.
    
    - Thoughts would be to then have another packge named themes?

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
     "log"
 
+    "cli-tutor/pkg/colours"
+
 	"github.com/muesli/termenv"
 )
 
@@ -12,24 +14,24 @@ func Print(s string, string_type string) {
 	styled := termenv.String(s)
 	switch string_type {
 	case "welcome":
-		fmt.Println(styled.Foreground(p.Color("#FF2FF2")))
-		log.Println(styled.Foreground(p.Color("#FF2FF2")))
+		fmt.Println(styled.Foreground(p.Color(colours.DefaultWelcome)))
+		log.Println(styled.Foreground(p.Color(colours.DefaultWelcome)))
 
 	case "error":
-		fmt.Println(styled.Foreground(p.Color("#FF0000")))
-		log.Println(styled.Foreground(p.Color("#FF0000")))
+		fmt.Println(styled.Foreground(p.Color(colours.DefaultError)))
+		log.Println(styled.Foreground(p.Color(colours.DefaultError)))
 
 	case "note":
-		fmt.Println(styled.Foreground(p.Color("#FFFF00")))
-		log.Println(styled.Foreground(p.Color("#FFFF00")))
+		fmt.Println(styled.Foreground(p.Color(colours.DefaultNote)))
+		log.Println(styled.Foreground(p.Color(colours.DefaultNote)))
 
 	case "tip":
-		fmt.Println(styled.Foreground(p.Color("#00FFF0")))
-		log.Println(styled.Foreground(p.Color("#00FFF0")))
+		fmt.Println(styled.Foreground(p.Color(colours.DefaultTip)))
+		log.Println(styled.Foreground(p.Color(colours.DefaultTip)))
     
     case "guide":
-		fmt.Println(styled.Foreground(p.Color("#00FF00")))
-		log.Println(styled.Foreground(p.Color("#00FF00")))
+		fmt.Println(styled.Foreground(p.Color(colours.DefaultGuide)))
+		log.Println(styled.Foreground(p.Color(colours.DefaultGuide)))
 
 	default:
 		fmt.Println(s)
